86fac6f5e8f9d8134fb4a1bd3c5ec69e171adb31
tracer-pa-public <tracer@njuics.org>
2019-09-17 18:56:15 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:56:15 up  3:05,  1 user,  load average: 0.05, 0.07, 0.04 995999142700377a915d4d2161aff01228c5747

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -67,7 +67,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
 uint32_t res = 0; 
-    res = dest + src+CF;
+    
 
     set_CF_add(res, src, data_size); 
     set_PF(res); 
@@ -75,6 +75,7 @@ uint32_t res = 0;
     set_ZF(res, data_size);    
     set_SF(res, data_size); 
     set_OF_add(res, src, dest, data_size);
+    res = dest + src+cpu.eflags.CF;
     return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
