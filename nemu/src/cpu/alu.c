e8ebed479060ca1a1eb669b74e70f8d6d7339058
tracer-pa-public <tracer@njuics.org>
2019-09-18 00:33:50 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:33:50 up  8:42,  1 user,  load average: 0.00, 0.00, 0.00 f9826d5f73bd5e2df751e25fd6f3dcca95c739d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -90,8 +90,8 @@ void set_CF_sub(uint32_t dest, uint32_t src, size_t data_size) {
     src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size); 
     cpu.eflags.CF = dest<src; 
 }
-void set_OF_sub(uint32_t src, uint32_t dest, size_t data_size){
-    uint32_t res=src+~dest+1;
+void set_OF_sub( uint32_t res, uint32_t src, uint32_t dest, size_t data_size){
+
     switch(data_size) { 
         case 8: 
             res= sign_ext(res & 0xFF, 8); 
@@ -106,7 +106,7 @@ void set_OF_sub(uint32_t src, uint32_t dest, size_t data_size){
             default: break;// do nothing 
     } 
     if(sign(src) != sign(dest)) { 
-               if(sign(src) != sign(res) )
+               if(sign(src) == sign(res) )
                             cpu.eflags.OF = 1; 
                else 
                             cpu.eflags.OF = 0;
@@ -131,7 +131,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
-	return __ref_alu_sbb(src, dest, data_size);
+	return __ref_alu_sbb(res,src, dest, data_size);
 #else
 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	assert(0);
