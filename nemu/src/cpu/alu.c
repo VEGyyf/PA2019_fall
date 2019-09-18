6e68bc2d5df8b501acc268e8161a0beef154e4fe
tracer-pa-public <tracer@njuics.org>
2019-09-18 17:19:15 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:19:15 up 19 min,  1 user,  load average: 0.01, 0.04, 0.07 3b597649ed85b6ba753637beff64bacd24352661

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -65,16 +65,10 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)//pass
 }
 void set_CF_adc(uint32_t result, uint32_t src, size_t data_size) { 
  if(cpu.eflags.CF){
-    uint32_t src_b=src;
-    src+=cpu.eflags.CF;
-    if(src<src_b){
-        cpu.eflags.CF=1;    
-    }
-    else{
+    result--;
     result = sign_ext(result & (0xFFFFFFFF>> (32 - data_size)), data_size); 
     src = sign_ext(src & (0xFFFFFFFF>> (32 - data_size)), data_size); 
     cpu.eflags.CF = result < src; 
-    }
  }
  else{
     result = sign_ext(result & (0xFFFFFFFF>> (32 - data_size)), data_size); 
