4c13807f3055d4b6e81422b34aca9d4fa17831a5
tracer-pa-public <tracer@njuics.org>
2019-09-22 23:35:14 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:35:14 up 9 min,  1 user,  load average: 0.05, 0.10, 0.04 50a66f64c61cfaecffcd4b2118e7376a2d5125

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -368,12 +368,12 @@ void set_CF(uint32_t dest, uint32_t src, size_t data_size) {
 }
 uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
 {
-
-    set_CF(res, src, data_size); 
-    set_PF(res); 
-    set_ZF(res, data_size);    
-    set_SF(res, data_size); 
 	dest<<=src;
+    set_CF(dest, src, data_size); 
+    set_PF(dest); 
+    set_ZF(dest, data_size);    
+    set_SF(dest, data_size); 
+
 	return dest& (0xFFFFFFFF >> (32 - data_size));
 
 }
