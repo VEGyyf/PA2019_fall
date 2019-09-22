5842a381dcc2bb8e5a23fbb7975e644d8e19d297
monitor <tracer@njuics.org>
2019-09-22 23:37:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -369,7 +369,7 @@ void set_CF(uint32_t dest, uint32_t src, size_t data_size) {
 uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
 {
     uint32_t res=0;
-	res=dest<<rc;
+	res=dest<<src;
     set_CF(res, src, data_size); 
     set_PF(res); 
     set_ZF(res, data_size);    
