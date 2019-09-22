0d391e5c24ceff992ea79265987bcfadd2382da7
tracer-pa-public <tracer@njuics.org>
2019-09-22 23:38:01 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:38:01 up 11 min,  1 user,  load average: 0.16, 0.09, 0.04 21d57c3316a2e29d6b99df736da3bb152ffd5ee0

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -333,16 +333,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	return 0;
 #endif
 }
-// CF contains information relevant to unsigned integers 
-void set_CF(uint32_t result, uint32_t src, size_t data_size) { 
-    result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size); 
-    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size); 
-    cpu.eflags.CF = result < src; 
-}
-void set_ZF(uint32_t result, size_t data_size) { 
-    result = result & (0xFFFFFFFF >> (32 - data_size)); 
-    cpu.eflags.ZF = (result == 0); 
-}
+
 // SF and OF contain information relevant to signed integers 
 void set_SF(uint32_t result, size_t data_size) { 
     result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size); 
