faf6a9014036c806ac5e1aadfd78463c1729faff
tracer-pa-public <tracer@njuics.org>
2019-09-23 16:49:58 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:49:58 up 19 min,  1 user,  load average: 0.22, 0.14, 0.10 fe9a0ffb32bf2921f2b6712528d114cfebff2b2

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -304,10 +304,10 @@ void set_CF_shl(uint32_t dest, uint32_t src, size_t data_size) {
     //dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size); 
     //src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size); 
     uint32_t count=1;
-    for(uint32_t i=0;i<data_size-src;i++)
+    for(uint32_t i=0;i<data_size-src-1;i++)
         count<<=1;
     uint32_t judge=dest&count;
-    judge>>=data_size-src;    
+    judge>>=data_size-src-1;    
     //judge = sign_ext(judge & (0xFFFFFFFF >> (32 - data_size)), data_size); 
     cpu.eflags.CF = (judge==1); 
 }
