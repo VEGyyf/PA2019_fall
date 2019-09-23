9e9bbbc95f1bfe43f802bdc3fc2b5bd8a29e81c2
tracer-pa-public <tracer@njuics.org>
2019-09-23 17:00:54 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:00:54 up 30 min,  1 user,  load average: 0.17, 0.10, 0.09 68e1da3bd55fb146cf2377c5c7e44c5885fa596

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -304,7 +304,7 @@ void set_CF_shl(uint32_t dest, uint32_t src, size_t data_size) {
     //dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size); 
     //src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size); 
     uint32_t count=1;
-    for(uint32_t i=0;i<data_size-ssrc;i++)
+    for(uint32_t i=0;i<data_size-src;i++)
         count<<=1;
     uint32_t judge=dest&count;
     judge>>=data_size-src;    
