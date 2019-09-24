11a61ffd1378a1261440199d3c500b3ef8390ca6
tracer-pa-public <tracer@njuics.org>
2019-09-24 18:54:28 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:54:28 up 25 min,  1 user,  load average: 0.28, 0.14, 0.14 6537de60d36ec0eff857f2d348db138c7465337

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -234,7 +234,7 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
     uint64_t res=dest_ext*src_ext;
     set_OF_mul(res, src, dest, data_size);
     set_CF_mul(res, src, data_size); 
-   return res & (0xFFFFFFFFFFFFFFFF >> (64- data_size));
+   return res;// & (0xFFFFFFFFFFFFFFFF >> (64- data_size));
 }
 
 int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
