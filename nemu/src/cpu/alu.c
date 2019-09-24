03ae0fed0d5e1c8a96f994d471b93a6da3f864ee
monitor <tracer@njuics.org>
2019-09-24 20:22:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -264,13 +264,9 @@ uint32_t alu_mod(uint64_t src, uint64_t dest)
 
 int32_t alu_imod(int64_t src, int64_t dest)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_imod(src, dest);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+    int32_t res=0;
+    res=dest%src;
+    return res;
 }
 
 uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size)//pass
