d4ccc85d3c62157c8b312b8ee081c4f1fb174302
tracer-pa-public <tracer@njuics.org>
2019-09-24 17:05:47 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:05:47 up 3 min,  1 user,  load average: 0.24, 0.25, 0.10 16d1bbb39ea5e205a9355fecf972a502bd80c6af

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -228,9 +228,11 @@ void set_OF_mul(uint32_t result, uint32_t src, uint32_t dest, size_t data_size){
             }
  }
 uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
-{
+{   
+    uint64_t src=sign_ext(src & 0xFFFFFFFF, 32); 
+    uint64_t dest=sign_ext(dest & 0xFFFFFFFF, 32); 
     uint64_t res=src*dest;
-    res = sign_ext(res & 0xFFFF, 64); 
+    res = sign_ext(res & 0xFFFFFFFF, 32); 
     set_OF_mul(res, src, dest, data_size);
     set_CF_mul(res, src, data_size); 
     return res;
