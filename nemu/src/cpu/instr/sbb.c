9a93675cecc025b10e3e99d6046e02834c253ca7
monitor <tracer@njuics.org>
2019-10-25 23:02:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/sbb.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	opr_dest.val = alu_sbb(opr_dest.val,opr_src.val,data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(sbb, r, rm, b)
+make_instr_impl_2op(sbb, r, rm, v)
+make_instr_impl_2op(sbb, rm, r, b)
+make_instr_impl_2op(sbb, rm, r, v)
+make_instr_impl_2op(sbb, i, a, b)
+make_instr_impl_2op(sbb, i, a, v)
