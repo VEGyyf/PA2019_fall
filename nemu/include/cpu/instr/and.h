0351cc3c5204c3001913010d7a4a3e7aaeae75d7
monitor <tracer@njuics.org>
2019-10-26 12:24:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/and.h
+++ nemu/include/cpu/instr/and.h
@@ -1,11 +1,11 @@
 #ifndef __INSTR_AND_H__
 #define __INSTR_AND_H__
 
-make_instr_impl_2op(and, r, rm, b);
-make_instr_impl_2op(and, r, rm, v);
-make_instr_impl_2op(and, rm, r, b);
-make_instr_impl_2op(and, rm, r, v);
-make_instr_impl_2op(and, i, a, b);
-make_instr_impl_2op(and, i, a, v);
+make_instr_func(and, r, rm, b);
+make_instr_func(and, r, rm, v);
+make_instr_func(and, rm, r, b);
+make_instr_func(and, rm, r, v);
+make_instr_func(and, i, a, b);
+make_instr_func(and, i, a, v);
 
 #endif
