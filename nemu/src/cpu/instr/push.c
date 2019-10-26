9192c0d21cc3c8e195f8fe62b9fd52cc663f8523
tracer-pa-public <tracer@njuics.org>
2019-10-26 17:29:29 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:29:29 up  6:33,  1 user,  load average: 0.21, 0.07, 0.02 5765b8e52004dc1cb04c627b837186947b65c1f0

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -13,40 +13,4 @@ static void instr_execute_1op()
 make_instr_impl_1op(push, r, v)
 make_instr_impl_1op(push, i, v)
 make_instr_impl_1op(push, i, b)
-make_instr_func(push_r_v)
-{
-        OPERAND src,dest;
-        src.type = OPR_REG;
-        src.sreg = SREG_CS;
-        src.data_size = data_size;
-        rel.addr = eip + 1;
 
-        operand_read(&rel);
-
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
-
-        cpu.eip += offset;
-
-        return 1 + data_size / 8;
-}
-
-make_instr_func(jmp_near)
-{
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = data_size;
-        rel.addr = eip + 1;
-
-        operand_read(&rel);
-
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
-
-        cpu.eip += offset;
-
-        return 1 + data_size / 8;
-}
