f18c15e3d724b91f0cd048059ff71379c6756ec2
monitor <tracer@njuics.org>
2019-10-26 12:29:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/sbb.h
+++ nemu/include/cpu/instr/sbb.h
@@ -1,11 +1,11 @@
 #ifndef __INSTR_SBB_H__
 #define __INSTR_SBB_H__
 
-make_instr_func(sbb, r, rm, b);
-make_instr_func(sbb, r, rm, v);
-make_instr_func(sbb, rm, r, b);
-make_instr_func(sbb, rm, r, v);
-make_instr_func(sbb, i, a, b);
-make_instr_func(sbb, i, a, v);
+make_instr_func(sbb_r2rm_b);
+make_instr_func(sbb_r2rm_v);
+make_instr_func(sbb_rm2r_b);
+make_instr_func(sbb_rm2r_v);
+make_instr_func(sbb_i2a_b);
+make_instr_func(sbb_i2a_v);
 
 #endif
--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -1,11 +1,11 @@
 #ifndef __INSTR_SUB_H__
 #define __INSTR_SUB_H__
 
-make_instr_func(sub, r, rm, b)
-make_instr_func(sub, r, rm, v)
-make_instr_func(sub, rm, r, b)
-make_instr_func(sub, rm, r, v)
-make_instr_func(sub,i,a,b)
-make_instr_func(sub,i,a,v)
+make_instr_func(sub_r2rm_b)
+make_instr_func(sub_r2rm_v)
+make_instr_func(sub_rm2r_b)
+make_instr_func(sub_rm2r_v)
+make_instr_func(sub_i2a_b)
+make_instr_func(sub_i2a_v)
 
 #endif
