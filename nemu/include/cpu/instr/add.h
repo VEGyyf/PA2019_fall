4edb04d7055c5366d6d14acdb254aa75739ec2dd
tracer-pa-public <tracer@njuics.org>
2019-10-26 12:58:29 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  12:58:29 up  2:02,  1 user,  load average: 0.08, 0.22, 0.21 3b7316cae0220bf64e9cbcc72aedc16062bc9ceb

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -1,7 +1,7 @@
 #ifndef __INSTR_ADD_H__
 #define __INSTR_ADD_H__
 
-make_instr_func(add_r2m_b);
+make_instr_func(add_r2rm_b);
 make_instr_func(add_r2rm_v);
 make_instr_func(add_rm2r_b);
 make_instr_func(add_rm2r_v);
