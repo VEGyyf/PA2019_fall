7cb2f6453b0bff7cf9eae71648e046ea8142e3c1
tracer-pa-public <tracer@njuics.org>
2019-10-26 11:50:31 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:50:31 up 54 min,  1 user,  load average: 0.51, 0.25, 0.14 1473f6337283e2d9dc7ab07d8fae0e1529442f44

--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -1,11 +1,11 @@
 #ifndef __INSTR_SUB_H__
 #define __INSTR_SUB_H__
 
-make_instr_impl_2op(aub, r, rm, b)
-make_instr_impl_2op(aub, r, rm, v)
-make_instr_impl_2op(aub, rm, r, b)
-make_instr_impl_2op(aub, rm, r, v)
-make_instr_impl_2op(aub,i,a,b)
-make_instr_impl_2op(aub,i,a,v)
+make_instr_impl_2op(sub, r, rm, b)
+make_instr_impl_2op(sub, r, rm, v)
+make_instr_impl_2op(sub, rm, r, b)
+make_instr_impl_2op(sub, rm, r, v)
+make_instr_impl_2op(sub,i,a,b)
+make_instr_impl_2op(sub,i,a,v)
 
 #endif
