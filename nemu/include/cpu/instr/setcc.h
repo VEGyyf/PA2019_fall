e9a8fc9ef928416e62eae0645771c3d978699307
tracer-pa-public <tracer@njuics.org>
2019-10-27 23:54:36 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:54:36 up  2:26,  1 user,  load average: 0.31, 0.18, 0.16 cb2a7cdabab21625569a898a24e74f68eb44b18

--- nemu/include/cpu/instr/setcc.h
+++ nemu/include/cpu/instr/setcc.h
@@ -1,21 +1,21 @@
 #ifndef __INSTR_SETCC_H__
 #define __INSTR_SETCC_H__
 
-make_instr_impl_1op_cc(setne_b);
-make_instr_impl_1op_cc(seta_b);
-make_instr_impl_1op_cc(setae_b);
-make_instr_impl_1op_cc(setbe_b);
-make_instr_impl_1op_cc(setc_b);
-make_instr_impl_1op_cc(sete_b);
-make_instr_impl_1op_cc(setg_b;
-make_instr_impl_1op_cc(setge_b;
-make_instr_impl_1op_cc(setl_b);
-make_instr_impl_1op_cc(setle_b);
-make_instr_impl_1op_cc(setno_b);
-make_instr_impl_1op_cc(setnp_b);
-make_instr_impl_1op_cc(setns_b);
-make_instr_impl_1op_cc(seto_b);
-make_instr_impl_1op_cc(setp_b);
-make_instr_impl_1op_cc(sets_b);
+make_instr_func(setne_b);
+make_instr_func(seta_b);
+make_instr_func(setae_b);
+make_instr_func(setbe_b);
+make_instr_func(setc_b);
+make_instr_func(sete_b);
+make_instr_func(setg_b;
+make_instr_func(setge_b;
+make_instr_func(setl_b);
+make_instr_func(setle_b);
+make_instr_func(setno_b);
+make_instr_func(setnp_b);
+make_instr_func(setns_b);
+make_instr_func(seto_b);
+make_instr_func(setp_b);
+make_instr_func(sets_b);
 
 #endif
