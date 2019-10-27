53176e84ff8f4f176ac1f15b38c27fbb6fdfe5eb
monitor <tracer@njuics.org>
2019-10-28 01:27:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/cmov.h
+++ nemu/include/cpu/instr/cmov.h
@@ -1,6 +1,20 @@
 #ifndef __INSTR_CMOV_H__
 #define __INSTR_CMOV_H__
 
-make_instr_func(cbw_a_v);
-
+make_instr_impl_2op_cc(cmova_rm2r_v);
+make_instr_impl_2op_cc(cmovae_rm2r_v);
+make_instr_impl_2op_cc(cmovb_rm2r_v);
+make_instr_impl_2op_cc(cmovbe_rm2r_v);
+make_instr_impl_2op_cc(cmovg_rm2r_v);
+make_instr_impl_2op_cc(cmovge_rm2r_v);
+make_instr_impl_2op_cc(cmovl_rm2r_v);
+make_instr_impl_2op_cc(cmovle_rm2r_v);
+make_instr_impl_2op_cc(cmovne_rm2r_v);
+make_instr_impl_2op_cc(cmovno_rm2r_v);
+make_instr_impl_2op_cc(cmovnp_rm2r_v);
+make_instr_impl_2op_cc(cmovns_rm2r_v);
+make_instr_impl_2op_cc(cmovo_rm2r_v);
+make_instr_impl_2op_cc(cmovp_rm2r_v);
+make_instr_impl_2op_cc(cmovs_rm2r_v);
+make_instr_impl_2op_cc(cmove_rm2r_v);
 #endif
