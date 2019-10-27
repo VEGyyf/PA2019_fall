3b9c9c527d1ea5acc1fddd91a1979e2ac65014cd
monitor <tracer@njuics.org>
2019-10-28 01:24:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/cbw.h
+++ nemu/include/cpu/instr/cbw.h
@@ -1,14 +1,6 @@
 #ifndef __INSTR_CBW_H__
 #define __INSTR_CBW_H__
 
-make_instr_func(and_r2rm_b);
-make_instr_func(and_r2rm_v);
-make_instr_func(and_rm2r_b);
-make_instr_func(and_rm2r_v);
-make_instr_func(and_i2a_b);
-make_instr_func(and_i2a_v);
-make_instr_func(and_i2rm_bv);
-make_instr_func(and_i2rm_v);
-make_instr_func(and_i2rm_b);
+make_instr_func(cbw_a_v);
 
 #endif
---
+++ nemu/include/cpu/instr/cmov.h
@@ -0,0 +1,6 @@
+#ifndef __INSTR_CMOV_H__
+#define __INSTR_CMOV_H__
+
+make_instr_func(cbw_a_v);
+
+#endif
