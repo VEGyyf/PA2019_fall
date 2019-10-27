381c617c4d64b837a41dd3feac136eccb5d4795b
tracer-pa-public <tracer@njuics.org>
2019-10-28 00:44:52 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:44:52 up  3:16,  1 user,  load average: 0.00, 0.02, 0.03 a4bb0387bdd2474eb318449590e1785648441b79

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -5,7 +5,7 @@ make_instr_func(lea)
        opr_dest.data_size = opr_src.data_size = data_size; // 指定操作数长度 
        int len = 1;
        len += modrm_r_rm(eip + 1,  &opr_dest, &opr_src); // 操作数寻址
-       operand_read(&opr_src); 
+       //operand_read(&opr_src); 
        opr_dest.val = opr_src.addr; 
        operand_write(&opr_dest);
        return len;
