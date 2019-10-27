c2108d60f43f4426e70f673aba7f60edd33b09e9
monitor <tracer@njuics.org>
2019-10-27 23:39:01 +0800

    git monitor commit.

---
+++ nemu/include/cpu/instr/shl.h
@@ -0,0 +1,11 @@
+#ifndef __INSTR_SHL_H__
+#define __INSTR_SHL_H__
+
+make_instr_impl_2op(shl_i_rm_b);
+make_instr_impl_2op(shl_i_rm_bv);
+make_instr_impl_2op(shl_c_rm_b);
+make_instr_impl_2op(shl_c_rm_bv);
+make_instr_impl_1op(shl_rm_b);
+make_instr_impl_1op(shl_rm_v);
+
+#endif
