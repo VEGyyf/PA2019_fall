965f72aa67f82cb4646d36a3f5e22c8a8f8051da
tracer-pa-public <tracer@njuics.org>
2019-10-27 14:43:57 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:43:57 up  3:51,  1 user,  load average: 0.05, 0.10, 0.06 e90466957455da2c710217f33b3b99e3ee65076

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -5,10 +5,10 @@ make_instr_func(ret_near)
     opr_dest.type=OPR_MEM;
     opr_dest.addr=cpu.esp;
 	operand_read(&opr_dest);//zhidingdezhandanyuan
-    opr_src.addr=eip;
+    /*opr_src.addr=eip;
     opr_src.type=OPR_REG;
     opr_src.val=opr_dest.val;
-    operand_write(&opr_src);
+    operand_write(&opr_src);*/
     cpu.esp+=4;
     cpu.eip=opr_dest.val;
     return 0 ;//the length of opr=0
