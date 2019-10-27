7f74f8142a567d3b4650126d168a51a8cde5faca
monitor <tracer@njuics.org>
2019-10-27 21:02:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -4,7 +4,7 @@ make_instr_func(lea)
 {
        opr_dest.data_size = opr_src.data_size = data_size; // 指定操作数长度 
        int len = 1;
-       len += modrm_r_rm(eip + 1, &opr_src, &opr_dest); // 操作数寻址
+       len += modrm_r_rm(eip + 1,  &opr_dest, &opr_src); // 操作数寻址
        //operand_read(&opr_src); 
        opr_dest.val = opr_src.addr; 
        operand_write(&opr_dest);
@@ -13,7 +13,8 @@ make_instr_func(lea)
 /*make_instr_func(mov_rm2r_b) { 
 OPERAND r, rm;
 r.data_size = rm.data_size = 8; // 指定操作数长度
-int len = 1; len += modrm_r_rm(eip + 1, &r, &rm); // 操作数寻址
+int len = 1; 
+len += modrm_r_rm(eip + 1, &r, &rm); // 操作数寻址
 operand_read(&rm);// mov 操作 r.val = rm.val; operand_write(&r);
 return len; 
 }
