60883266da2e1f1d621ebe048c428b90c24a3f06
monitor <tracer@njuics.org>
2019-10-28 18:37:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -49,7 +49,7 @@ instr_func opcode_entry[256] = {
     /* 0xb4 - 0xb7*/ mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
     /* 0xb8 - 0xbb*/ mov_i2r_v, mov_i2r_v, mov_i2r_v, mov_i2r_v,
     /* 0xbc - 0xbf*/ mov_i2r_v, mov_i2r_v, mov_i2r_v, mov_i2r_v,
-    /* 0xc0 - 0xc3*/ group_2_b, group_2_v, __ref_ret_near_imm16, ret_near,
+    /* 0xc0 - 0xc3*/ group_2_b, group_2_v, ret_near_imm16, ret_near,
     /* 0xc4 - 0xc7*/ inv, inv, mov_i2rm_b, mov_i2rm_v,
     /* 0xc8 - 0xcb*/ inv, leave, inv, inv,
     /* 0xcc - 0xcf*/ inv, inv, inv, inv,
@@ -60,11 +60,11 @@ instr_func opcode_entry[256] = {
     /* 0xe0 - 0xe3*/ inv, inv, inv, jecxz_short_,
     /* 0xe4 - 0xe7*/ inv, inv, inv, inv,
     /* 0xe8 - 0xeb*/ call_near, jmp_near, jmp_far_imm, jmp_short,
-    /* 0xec - 0xef*/ __ref_in_b, __ref_in_v, __ref_out_b, __ref_out_v,
-    /* 0xf0 - 0xf3*/ inv, break_point, inv, __ref_rep_repe,
-    /* 0xf4 - 0xf7*/ __ref_hlt, inv, group_3_b, group_3_v,
-    /* 0xf8 - 0xfb*/ __ref_clc, inv, __ref_cli, __ref_sti,
-    /* 0xfc - 0xff*/ __ref_cld, inv, inv, group_5_indirect,
+    /* 0xec - 0xef*/ inv,inv,inv,inv,
+    /* 0xf0 - 0xf3*/ inv, break_point, inv, inv,
+    /* 0xf4 - 0xf7*/ inv, inv, group_3_b, group_3_v,
+    /* 0xf8 - 0xfb*/ inv, inv, inv,inv,
+    /* 0xfc - 0xff*/ inv, inv, inv, group_5_indirect,
 };
 /* 0x80 */
 instr_func group_1_b_entry[8] =
