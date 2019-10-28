a8b971e71bb8c3b30788a6d2b15611b3494703c3
monitor <tracer@njuics.org>
2019-10-28 11:39:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,7 +2,7 @@
 
 make_instr_func(leave)
 {
-    int len = 1;                                                                                                                       \
+    int len = 1;           //mov ebp, esp;                                                                                                            \
     OPERAND src,dest;
     src.type=dest.type=OPR_REG;
     src.data_size=dest.data_size=data_size;
@@ -11,7 +11,13 @@ make_instr_func(leave)
     operand_read(src);
 	dest.val = src.val;
 	operand_write(dest);                                                                                                 \
-		                                                                                                             \
+	opr_dest.data_size=data_size;// pop ebp;
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+	operand_read(&opr_dest);
+    src.val=opr_dest.val;
+    operand_write(&opr_src);
+	cpu.esp+=4;                                                                                                          \
     return len;     
 }
 
