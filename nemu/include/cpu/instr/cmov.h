d3b75b935eefa31c7d7f0691373331ebe7d6d914
tracer-pa-public <tracer@njuics.org>
2019-10-28 09:09:18 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:09:18 up 11:41,  1 user,  load average: 0.28, 0.11, 0.04 7bf6626354b7d94fe7a56fb8401be026acd6718

--- nemu/include/cpu/instr/cmov.h
+++ nemu/include/cpu/instr/cmov.h
@@ -1,20 +1,20 @@
 #ifndef __INSTR_CMOV_H__
 #define __INSTR_CMOV_H__
 
-make_instr_impl_2op_cc(cmova_rm2r_v);
-make_instr_impl_2op_cc(cmovae_rm2r_v);
-make_instr_impl_2op_cc(cmovb_rm2r_v);
-make_instr_impl_2op_cc(cmovbe_rm2r_v);
-make_instr_impl_2op_cc(cmovg_rm2r_v);
-make_instr_impl_2op_cc(cmovge_rm2r_v);
-make_instr_impl_2op_cc(cmovl_rm2r_v);
-make_instr_impl_2op_cc(cmovle_rm2r_v);
-make_instr_impl_2op_cc(cmovne_rm2r_v);
-make_instr_impl_2op_cc(cmovno_rm2r_v);
-make_instr_impl_2op_cc(cmovnp_rm2r_v);
-make_instr_impl_2op_cc(cmovns_rm2r_v);
-make_instr_impl_2op_cc(cmovo_rm2r_v);
-make_instr_impl_2op_cc(cmovp_rm2r_v);
-make_instr_impl_2op_cc(cmovs_rm2r_v);
-make_instr_impl_2op_cc(cmove_rm2r_v);
+make_instr_impl_func(cmova_rm2r_v);
+make_instr_impl_func(cmovae_rm2r_v);
+make_instr_impl_func(cmovb_rm2r_v);
+make_instr_impl_func(cmovbe_rm2r_v);
+make_instr_impl_func(cmovg_rm2r_v);
+make_instr_impl_func(cmovge_rm2r_v);
+make_instr_impl_func(cmovl_rm2r_v);
+make_instr_impl_func(cmovle_rm2r_v);
+make_instr_impl_func(cmovne_rm2r_v);
+make_instr_impl_func(cmovno_rm2r_v);
+make_instr_impl_func(cmovnp_rm2r_v);
+make_instr_impl_func(cmovns_rm2r_v);
+make_instr_impl_func(cmovo_rm2r_v);
+make_instr_impl_func(cmovp_rm2r_v);
+make_instr_impl_func(cmovs_rm2r_v);
+make_instr_impl_func(cmove_rm2r_v);
 #endif
