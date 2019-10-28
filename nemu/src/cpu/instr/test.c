85c78b8a4f59a25e9221f7e948625871f76d8977
tracer-pa-public <tracer@njuics.org>
2019-10-28 09:11:27 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:11:27 up 11:43,  1 user,  load average: 0.32, 0.17, 0.06 92967f7672f6f597e159d7e1b789bbd379a6ab76

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -3,7 +3,7 @@
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
-    operand_read(&opr_dst);
+    operand_read(&opr_dest);
     uint32_t temp = alu_and(opr_src.val,opr_dest.val,opr_dest.data_size);
 	operand_write(&opr_dest);
 }
