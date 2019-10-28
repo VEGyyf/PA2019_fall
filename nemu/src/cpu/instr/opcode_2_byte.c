f13f48a77cb2acef08e63b6da9bcd1a49b7d9b14
tracer-pa-public <tracer@njuics.org>
2019-10-28 11:08:41 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:08:41 up 2 min,  1 user,  load average: 0.66, 0.41, 0.17 72a3ed9f98f4a01545475b441b72ae0b678a145c

--- nemu/src/cpu/instr/opcode_2_byte.c
+++ nemu/src/cpu/instr/opcode_2_byte.c
@@ -4,10 +4,10 @@ make_instr_func(opcode_2_byte)
 {
 	int len = 1;
 	uint8_t op = instr_fetch(eip + 1, 1);
-#ifdef NEMU_REF_INSTR
+/*#ifdef NEMU_REF_INSTR
 	len += __ref_opcode_2_byte_entry[op](eip + 1, op);
-#else
+#else*/
 	len += opcode_2_byte_entry[op](eip + 1, op);
-#endif
+//#endif
 	return len;
 }
