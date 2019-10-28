09f7f61a35c5e924805aafdb9fb5ac0ceb40683e
monitor <tracer@njuics.org>
2019-10-28 09:35:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -3,14 +3,7 @@
 static void instr_execute_1op() 
 {
    
-	operand_read(&opr_src);
-	cpu.esp-=4;
-    opr_dest.data_size=data_size;
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-    opr_dest.val=opr_src.val;
-    //len + = modrm_r_rm(eip + 1, &opr_src, &opr_dest); 
-    operand_write(&opr_dest);
+	opr_src.val=~opr_src.val;
 }
 
 make_instr_impl_1op(not, rm, b)
