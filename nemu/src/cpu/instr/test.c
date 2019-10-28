6a73d59b4b2712ac687b69f52596284eb2fbe017
tracer-pa-public <tracer@njuics.org>
2019-10-28 09:27:36 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:27:36 up 11:59,  1 user,  load average: 0.01, 0.15, 0.15 86f0888f5846d773528c6752cf6dff238b3729e

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -10,9 +10,9 @@ static void instr_execute_2op()
 	operand_write(&opr_dest);
 }
 
-make_instr_impl_2op( test, r, rm, b)
-make_instr_impl_2op( test, r, rm, v)
-make_instr_impl_2op( test,i,a,b)
-make_instr_impl_2op( test,i,a,v)
-make_instr_impl_2op( test,i,rm,v)
-make_instr_impl_2op( test,i,rm,b)
+make_instr_impl_2op(test, r, rm, b)
+make_instr_impl_2op(test, r, rm, v)
+make_instr_impl_2op(test,i,a,b)
+make_instr_impl_2op(test,i,a,v)
+make_instr_impl_2op(test,i,rm,v)
+make_instr_impl_2op(test,i,rm,b)
