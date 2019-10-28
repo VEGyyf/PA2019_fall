3dca203fbbbc8966436963257eaaa6fe9e6bff6d
tracer-pa-public <tracer@njuics.org>
2019-10-28 20:08:47 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:08:47 up  7:17,  1 user,  load average: 0.20, 0.13, 0.12 fa7d7be9cad2e5c46e73e11fa6275ba672b9a1b2

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -6,9 +6,9 @@ static void instr_execute_2op()
 	operand_read(&opr_src);
     operand_read(&opr_dest);
     //uint32_t src=opr_src.val;
-    opr_src.val=sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - opr_src.data_size)),data_size);
-    opr_dest.val=sign_ext(opr_dest.val & (0xFFFFFFFF >> (32 - opr_dest.data_size)),data_size);
-	opr_dest.val = alu_and(opr_src.val,opr_dest.val,opr_dest.data_size);
+    opr_src.val=sign_ext(opr_src.val & (0xFFFFFFFF >> (32 - opr_src.data_size)),32);
+    opr_dest.val=sign_ext(opr_dest.val & (0xFFFFFFFF >> (32 - opr_dest.data_size)),32);
+	opr_dest.val = alu_and(opr_src.val,opr_dest.val,32);
 	operand_write(&opr_dest);
 }
 
