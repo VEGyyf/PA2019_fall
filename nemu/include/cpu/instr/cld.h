772ebda53c2dd19deef21187bcd8c81396231fa2
monitor <tracer@njuics.org>
2019-10-29 19:30:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/cld.h
+++
@@ -1,6 +0,0 @@
-#ifndef __INSTR_CLD_H__
-#define __INSTR_CLD_H__
-
-make_instr_func(cld);
-
-#endif
--- nemu/src/cpu/instr/cld.c
+++
@@ -1,7 +0,0 @@
-#include "cpu/instr.h"
-
-make_instr_func(cld)
-{
-    cpu.eflags.DF=0;
-    return 1;
-}
