0443a533f0723000c6c1f2bc6798d06f4a465c93
monitor <tracer@njuics.org>
2019-10-29 19:17:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -32,13 +32,13 @@ make_instr_func(call_near_indirect)
         opr_dest.val=eip+5;
         operand_write(&opr_dest);//push ret_addr
 
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = data_size;
-        rel.addr = eip + 1;
+        OPERAND obj;
+        obj.sreg = SREG_CS;
+        obj.data_size = data_size;
+        int len=1;
+        len+=modrm_rm(eip+1,&obj);
 
-        operand_read(&rel);
+        operand_read(&obj);
 
         int offset = sign_ext(rel.val, data_size);
         print_asm_1("call", "", 1 + data_size / 8, &rel);
