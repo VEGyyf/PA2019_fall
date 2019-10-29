518eb36d0cbb65eed90d7bbe0446f413dab1c77d
tracer-pa-public <tracer@njuics.org>
2019-10-29 21:14:44 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:14:44 up  4:07,  1 user,  load average: 0.29, 0.56, 0.58 1b730c1e13239051c3f69719dda038dabcebea24

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -14,15 +14,8 @@ make_instr_func(leave)
 	operand_read(&dest);
     cpu.ebp=dest.val;
     
-	cpu.esp+=4;    //pop ebp
+	cpu.esp+=data_size/8;    //pop ebp
      print_asm_0("leave","",len);                                                                                                     
     return len;     
 }
-/*pop:
-   opr_dest.data_size=data_size;
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-	operand_read(&opr_dest);//zhidingdezhandanyuan
-    opr_src.val=opr_dest.val;
-    operand_write(&opr_src);
-	cpu.esp+=4;*/
+
