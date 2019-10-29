a95a4fb636ae2f23a6bf491bc8ef9e480c13d7a4
tracer-pa-public <tracer@njuics.org>
2019-10-29 19:18:26 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  19:18:26 up  2:10,  1 user,  load average: 0.17, 0.37, 0.38 15009bdc7f259a937f4d3b1a4db4ba0a764390c

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -43,9 +43,9 @@ make_instr_func(call_near_indirect)
         //int offset = sign_ext(rel.val, data_size);
         print_asm_1("call", "", len, &obj);
 
-        cpu.eip += offset;
+        cpu.eip =obj.val;
 
-        return 1 + data_size / 8;//=jmp,跳转到指定地址
+        return 0;//=jmp,跳转到指定地址
 }
 
 
