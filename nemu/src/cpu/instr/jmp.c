760e5bdfe510b466e00fadf25e19b0373fe158be
tracer-pa-public <tracer@njuics.org>
2019-10-29 19:06:21 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  19:06:21 up  1:58,  1 user,  load average: 0.68, 0.36, 0.32 51247075934b3ab52d84c9dee8cca3fcf5e84ef8

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -63,7 +63,7 @@ make_instr_func(jmp_near_indirect)
         obj.type = OPR_MEM;
         //obj.sreg = SREG_CS;
         obj.data_size = data_size;
-        obj.addr = eip + 3;
+        obj.addr = eip + 1;
 
         operand_read(&obj);
 
