ababee4c3155d0ba0fbe00a097eeda06dad615d9
monitor <tracer@njuics.org>
2019-11-29 22:46:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -11,8 +11,23 @@ void init_cache(){
 }
 //初始化 cache ，核心就是把 valid bit 都清 0
 
-uint32_t cache_read paddr_t paddr , size_t len , CacheLine *cache){
-
+uint32_t cache_read (paddr_t paddr , size_t len , CacheLine *cache){
+        uint32_t res;
+        uint32_t mark_paddr=(0xFFFFE000&paddr);
+        mark_paddr>>=13;
+        uint32_t group=(0x00001FC0&paddr);
+        group>>=6;
+        uint32_t addrinblock=(0x0000003F&paddr);
+        for(uint32_t i=(group<<3);i<8;i++){
+            if(cache[i].mark==mark_paddr){
+                if(cache[i].valid_bit){
+                    
+                }
+            }
+            else{//duneicun
+                
+            }
+        }
 }
 //读 cache
 //根据 paddr 找到标记、组号、与块内地址
@@ -21,7 +36,7 @@ uint32_t cache_read paddr_t paddr , size_t len , CacheLine *cache){
 //命中怎样？不命中（缺失）又怎样？
 //组满了怎办？（ 随机替换算法)
 
-void cache_write paddr_t paddr , size_t len , uint32_t data, CacheLine *cache){
+void cache_write (paddr_t paddr , size_t len , uint32_t data, CacheLine *cache){
 
 }
 //写 cache
