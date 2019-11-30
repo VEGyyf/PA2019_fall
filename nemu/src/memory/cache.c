f43a70d160204e5314c5bea1af9b111ed0e55523
tracer-pa-public <tracer@njuics.org>
2019-11-30 09:01:24 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:01:24 up  7:33,  1 user,  load average: 0.04, 0.10, 0.14 cf96b6efe20a5b6c3a52f6d293ddd14fb6a7efe5

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -43,7 +43,7 @@ uint32_t cache_read (paddr_t paddr , size_t len , CacheLine *cache){
                     uint32_t j=i+1;
                     //void* src2=(void*)(&cache[j].data);
                     memcpy(&part2,&cache[j].data,len2);
-                    part2<<=len2;
+                    part2<<=len1;
                     res=part2|part1;
                }
             }
