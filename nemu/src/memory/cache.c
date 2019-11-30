915695620d07d1c91ca988b17b155625c011550d
monitor <tracer@njuics.org>
2019-11-30 17:34:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -55,8 +55,8 @@ uint32_t cache_read (paddr_t paddr , size_t len , CacheLine *cache){
                 memcpy(&cache[index+line].data,hw_mem+paddr-addrinblock,64);//把主存块搬到cache
         }
         memcpy(alldata,&cache[index+line].data,64);
-        if(addrinblock+len>64)*(alldata+1)=cache_read(paddr-addrinblock+64,64,cache);//跨行
-        alldata=*(uint8_t*)alldata;
+        if(addrinblock+len>64)*(alldata+16)=cache_read(paddr-addrinblock+64,64,cache);//跨行
+        //alldata=*(uint8_t*)alldata;
         memcpy(&res,alldata+addrinblock,len);
     return res;
 
