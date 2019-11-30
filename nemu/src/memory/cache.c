f5a126684db8f3b0b861ab0d7a431f4f8649f439
monitor <tracer@njuics.org>
2019-11-30 16:54:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -133,7 +133,7 @@ uint32_t res=0;
                     uint32_t j=i+1;
                     //void* src2=(void*)(&cache[j].data);
                     memcpy(&part2,&cache[j].data,len2);
-                    part2<<=len1;
+                    part1<<=len2;
                     res=part2|part1;
                }
             }
