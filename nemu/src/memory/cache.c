b9a39f824b4d7319a5c82b1e2fe033f82bb3971b
monitor <tracer@njuics.org>
2019-11-30 17:25:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -18,7 +18,7 @@ void init_cache(){
 //初始化 cache ，核心就是把 valid bit 都清 0
 
 uint32_t cache_read (paddr_t paddr , size_t len , CacheLine *cache){
-        uint32_t res=0;
+        /*uint32_t res=0;
         uint32_t tag_paddr=(0xFFFFE000&paddr);
         tag_paddr>>=13;
         uint32_t group=(0x00001FC0&paddr);
@@ -57,7 +57,7 @@ uint32_t cache_read (paddr_t paddr , size_t len , CacheLine *cache){
         memcpy(alldata,cache[index+line].data,64);
         if(addrinblock+len>64)*(uint32_t*)(alldata+64)=cache_read(paddr-addrinblock+64,64,cache);//跨行
         memcpy(&res,alldata+addrinblock,len);
-    return res;
+    return res;*/
 
         
         /*uint32_t res=0;
