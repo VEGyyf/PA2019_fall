beda56b5c028a0377c9add9256d281eee737279f
monitor <tracer@njuics.org>
2019-12-03 15:40:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -21,19 +21,16 @@ make_instr_func(jmp_near)
 
 make_instr_func(jmp_far_imm)//TODO:change CS,段间绝对转移
 {
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = data_size;
-        rel.addr = eip + 1;
+       OPERAND obj;
+        obj.sreg = SREG_CS;
+        obj.data_size = data_size;
+        int len=1;
+        len+=modrm_rm(eip+1,&obj);
+        operand_read(&obj);
 
-        operand_read(&rel);
-
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
-
-        cpu.eip += offset;
+        print_asm_1("jmp", "", len, &obj);
+        
+        cpu.eip = obj.val;
 
         return 0;
 }
