c96806b77df6cd20fbb2cd11765cf1e19d0de7dd
monitor <tracer@njuics.org>
2019-12-03 19:49:01 +0800

    git monitor commit.

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -63,13 +63,22 @@ uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
             laddr = segment_translate(vaddr, sreg);
         }
         return laddr_read(laddr, len);
-#endif
+    #endif
 }
 
 void vaddr_write(vaddr_t vaddr, uint8_t sreg, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
-	laddr_write(vaddr, len, data);
+	//laddr_write(vaddr, len, data);
+    #ifndef IA32_SEG
+        return laddr_write(vaddr, len, data);
+    #else
+        uint32_t laddr = vaddr;
+        if( /* what condition??? */){
+            laddr = segment_translate(vaddr, sreg);
+        }
+        return laddr_write(laddr, len, data);
+    #endif
 }
 
 void init_mem()
