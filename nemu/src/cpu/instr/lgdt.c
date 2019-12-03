60d7bdb928cc010e1b822c4497c6eff5d657b69c
tracer-pa-public <tracer@njuics.org>
2019-12-03 21:25:51 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:25:51 up  7:13,  1 user,  load average: 0.09, 0.18, 0.25 6ca8e09fc8ab7d1a795e55240b6601fb44e6039

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -20,7 +20,7 @@ make_instr_func(lgdt)
     }
     else if(data_size==32)memcpy(&bs,&m.val+2,4);
     cpu.gdtr.base=bs;   
-
+    assert(lmt==0xfffff);
 	print_asm_1("lgdt", "", len, &m);
 	return len;
 }
