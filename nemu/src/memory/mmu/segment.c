4d3f38eccd36a64654a7d763ae6d844147405c41
monitor <tracer@njuics.org>
2019-12-03 19:46:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -34,6 +34,13 @@ void load_sreg(uint8_t sreg)
     uint32_t bs=base_15_0|base_23_16;
     bs=bs|base_31_24;
     temp.base=bs;
+
+    uint32_t limit_15_0=addr.limit_15_0;
+    uint32_t limit_19_16=addr.limit_19_16;
+    limit_19_16<<=16;
+    uint32_t lmt=limit_19_16|limit_15_0;
+    temp.limit=lmt;    
+
     temp.privilege_level=addr.privilege_level;
     temp.type=addr.type;
     temp.soft_use=addr.soft_use;
