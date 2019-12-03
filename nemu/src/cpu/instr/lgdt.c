967d6e2fdf7170552674173d5770c00b13c91aef
monitor <tracer@njuics.org>
2019-12-03 14:22:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -8,11 +8,9 @@ make_instr_func(lgdt)
 
 /*ELSE (* instruction = LGDT *)
 IF OperandSize = 16
-← m16:
-THEN GDTR.Limit:Base
-24 (* 24 bits of base loaded *)
-← m16:
-ELSE GDTR.Limit:Base
-32;
+
+THEN GDTR.Limit:Base    ← m16:24 (* 24 bits of base loaded *)
+
+ELSE GDTR.Limit:Base    ← m16:32;
 FI;
 FI;*/
