a0e70803a9a669f864ceaae67a5c680074553af8
monitor <tracer@njuics.org>
2020-01-12 15:54:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pusha.c
+++ nemu/src/cpu/instr/pusha.c
@@ -2,15 +2,26 @@
 
 make_instr_func(pusha)
 {
-	operand_read(&opr_src);
-   
-	if(opr_src->data_size==16){
+	if(data_size==16){
         
                     
             
     }
-    else if(opr_src->data_size==32){
+    else if(data_size==32){
          uint32_t temp=cpu.esp;
+/	operand_read(&opr_src);
+	cpu.esp-=data_size/8;
+    opr_dest.data_size=data_size;
+    opr_dest.type=OPR_MEM;
+    opr_dest.addr=cpu.esp;
+    opr_dest.val=opr_src.val;
+    //len + = modrm_r_rm(eip + 1, &opr_src, &opr_dest); 
+    operand_write(&opr_dest);
+/   cpu.esp-=data_size/8;
+        opr_dest.type=OPR_MEM;
+        opr_dest.addr=cpu.esp;
+        opr_dest.val=eip+5;
+        operand_write(&opr_dest);//push ret_addr
     }
 
     
