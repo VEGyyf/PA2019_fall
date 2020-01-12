d99c7dd4a35411d3d985191a169a7ab715b4bf87
monitor <tracer@njuics.org>
2020-01-12 16:10:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pusha.c
+++ nemu/src/cpu/instr/pusha.c
@@ -3,24 +3,43 @@
 make_instr_func(pusha)
 {
 	if(data_size==16){
-        
-                    
-            
+    uint16_t temp=cpu.esp;//Temp ← (ESP);
+	cpu.esp-=data_size/8;
+    opr_dest.data_size=data_size;
+    opr_dest.type=OPR_REG;
+    opr_dest.addr=cpu.esp;
+    opr_dest.sreg=SREG_SS;
+    opr_dest.val=cpu.ax;
+    operand_write(&opr_dest);//Push(EAX);
+    opr_dest.val=cpu.cx;
+    operand_write(&opr_dest);//Push(ECX);
+    opr_dest.val=cpu.dx;
+    operand_write(&opr_dest);//Push(EDX);
+    opr_dest.val=cpu.bx;
+    operand_write(&opr_dest);//Push(EBX);
+    opr_dest.val=cpu.temp;
+    operand_write(&opr_dest);//Push(Temp);
+    opr_dest.val=cpu.bp;
+    operand_write(&opr_dest);//Push(EBP);
+    opr_dest.val=cpu.si;
+    operand_write(&opr_dest);//Push(ESI);
+    opr_dest.val=cpu.di;
+    operand_write(&opr_dest);//Push(EDI);       
     }
     else if(data_size==32){
     uint32_t temp=cpu.esp;//Temp ← (ESP);
 	cpu.esp-=data_size/8;
     opr_dest.data_size=data_size;
-    opr_dest.type=OPR_MEM;
+    opr_dest.type=OPR_REG;
     opr_dest.addr=cpu.esp;
     opr_dest.sreg=SREG_SS;
     opr_dest.val=cpu.eax;
     operand_write(&opr_dest);//Push(EAX);
     opr_dest.val=cpu.ecx;
     operand_write(&opr_dest);//Push(ECX);
-    opr_dest.val=cpu.ecx;
-    operand_write(&opr_dest);//Push(EDX);
     opr_dest.val=cpu.edx;
+    operand_write(&opr_dest);//Push(EDX);
+    opr_dest.val=cpu.ebx;
     operand_write(&opr_dest);//Push(EBX);
     opr_dest.val=cpu.temp;
     operand_write(&opr_dest);//Push(Temp);
@@ -30,8 +49,6 @@ make_instr_func(pusha)
     operand_write(&opr_dest);//Push(ESI);
     opr_dest.val=cpu.edi;
     operand_write(&opr_dest);//Push(EDI);
-
-
     }
 
     
