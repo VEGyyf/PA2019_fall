78d68f441568c44b452e5673ce092507460445bd
monitor <tracer@njuics.org>
2020-01-12 16:25:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/popa.c
+++ nemu/src/cpu/instr/popa.c
@@ -8,20 +8,32 @@ make_instr_func(popa)
     }
     else if(data_size=32){
     opr_dest.data_size=data_size;
-    opr_dest.type=OPR_MEM;
+    opr_dest.type=OPR_MEM;//reg?
     opr_dest.addr=cpu.esp;
+    opr_dest.sreg=SREG_SS;
 	operand_read(&opr_dest);//zhidingdezhandanyuan
     cpu.edi=opr_dest.val;
-	cpu.esp+=data_size/8;
-/
-    uint8_t len=1;
-    opr_dest.data_size=data_size;//!!initialize
-    opr_dest.type=OPR_MEM;
-    opr_dest.addr=cpu.esp;
-	operand_read(&opr_dest);//zhidingdezhandanyuan
-
-    cpu.eip=opr_dest.val;
-    cpu.esp+=data_size/8;
+	cpu.esp+=data_size/8;//EDI ← Pop();
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+    cpu.esi=opr_dest.val;
+	cpu.esp+=data_size/8;//ESI ← Pop();
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+    cpu.ebp=opr_dest.val;
+	cpu.esp+=data_size/8;//EBP ← Pop();
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+	cpu.esp+=data_size/8;//throwaway ← Pop (); (* Skip ESP *)
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+    cpu.ebx=opr_dest.val;
+	cpu.esp+=data_size/8;//EBX ← Pop();
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+    cpu.edx=opr_dest.val;
+	cpu.esp+=data_size/8;//EDX ← Pop();
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+    cpu.ecx=opr_dest.val;
+	cpu.esp+=data_size/8;//ECX ← Pop();
+    operand_read(&opr_dest);//zhidingdezhandanyuan
+    cpu.eax=opr_dest.val;
+	cpu.esp+=data_size/8;//EAX ← Pop();
     }
     return 0 ;//the length of opr=0
 }
