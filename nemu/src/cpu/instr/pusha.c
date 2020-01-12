a85598a34fc10abca3937685e66118a64761efea
monitor <tracer@njuics.org>
2020-01-12 14:39:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pusha.c
+++ nemu/src/cpu/instr/pusha.c
@@ -1,20 +1,35 @@
 #include "cpu/instr.h"
 
-static void instr_execute_1op() 
+make_instr_func(pusha)
 {
-   
 	operand_read(&opr_src);
 	cpu.esp-=data_size/8;
     opr_dest.data_size=data_size;
     opr_dest.type=OPR_MEM;
     opr_dest.addr=cpu.esp;
     opr_dest.val=opr_src.val;
-    //len + = modrm_r_rm(eip + 1, &opr_src, &opr_dest); 
     operand_write(&opr_dest);
 }
-
-make_instr_impl_1op(push, r, v)
-make_instr_impl_1op(push, i, v)
-make_instr_impl_1op(push, i, b)
-make_instr_impl_1op(push, rm, v)
-
+/*IF OperandSize = 16 (* PUSHA instruction *)
+THEN
+Temp ← (SP);
+Push(AX);
+Push(CX);
+Push(DX);
+Push(BX);
+Push(Temp);
+Push(BP);
+Push(SI);
+Push(DI);
+ELSE (* OperandSize = 32, PUSHAD instruction *)
+Temp ← (ESP);
+Push(EAX);
+Push(ECX);
+Push(EDX);
+Push(EBX);
+Push(Temp);
+Push(EBP);
+Push(ESI);
+Push(EDI);
+FI;
+*/
