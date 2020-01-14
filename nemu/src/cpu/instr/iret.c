49fb981e7e0cbba100fde1e6e0af2d36912f6b3e
monitor <tracer@njuics.org>
2020-01-14 21:15:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -2,12 +2,15 @@
 
 make_instr_func(iret)
 {
-    
+    //eip<--pop()
+    //cs<--pop()
+    //eflags<--pop()
     print_asm_0("iret", "", 1);
     return 0 ;
 }
 /*
 通过iret 指令恢复最初被保护的程序断点和状态信息，返回原程序被中断的指令（或下一条，根据保护断点时具体保存的EIP 决定）继续执行。
+
 eip<--pop()
 cs<--pop()
 eflags<--pop()
