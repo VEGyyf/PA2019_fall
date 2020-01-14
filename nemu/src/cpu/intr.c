1bbbf0d0a057cc13f6b5f3fb2b528c3541957d65
monitor <tracer@njuics.org>
2020-01-14 22:05:01 +0800

    git monitor commit.

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,7 +7,36 @@ void raise_intr(uint8_t intr_no)
 #ifdef IA32_INTR
 	//printf("Please implement raise_intr()");
 	//assert(0);
-
+	cpu.esp-=data_size/8;
+    opr_dest.data_size=data_size;
+    opr_dest.type=OPR_REG;
+    opr_dest.addr=cpu.esp;
+    opr_dest.sreg=SREG_SS;
+    opr_dest.val=cpu.eflags;
+    operand_write(&opr_dest);//Push(EFLAGS);
+	cpu.esp-=data_size/8;
+    opr_dest.addr=cpu.esp;
+    opr_dest.sreg=SREG_CS;
+    opr_dest.val=cpu.cs;
+    operand_write(&opr_dest);//Push(ECX);
+	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
+    opr_dest.val=cpu.edx;
+    operand_write(&opr_dest);//Push(EDX);
+	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
+    opr_dest.val=cpu.ebx;
+    operand_write(&opr_dest);//Push(EBX);
+	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
+    opr_dest.val=cpu.temp;
+    operand_write(&opr_dest);//Push(Temp);
+	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
+    opr_dest.val=cpu.ebp;
+    operand_write(&opr_dest);//Push(EBP);
+	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
+    opr_dest.val=cpu.esi;
+    operand_write(&opr_dest);//Push(ESI);
+	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
+    opr_dest.val=cpu.edi;
+    operand_write(&opr_dest);//Push(EDI);
 
 
 // Trigger an exception/interrupt with 'intr_no'
