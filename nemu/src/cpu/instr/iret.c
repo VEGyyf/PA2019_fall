6d3e41de4191cd6448dc3d8c0c93e6e42f904f95
monitor <tracer@njuics.org>
2020-01-14 20:58:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -2,10 +2,11 @@
 
 make_instr_func(iret)
 {
-
+    
     print_asm_0("iret", "", 1);
     return 1 ;
 }
 /*
-
+当使用IRET指令返回到相同保护级别的任务时，IRET会从堆栈弹出代码段选择子及指令指针分别到CS与IP寄存器，并弹出标志寄存器内容到EFLAGS寄存器。
+?16?32?va?pa?pe?
 */
