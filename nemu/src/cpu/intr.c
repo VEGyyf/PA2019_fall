f1eaf200c8a2b0b7ba1510f44dfce5f6efb6b23c
tracer-pa-public <tracer@njuics.org>
2020-01-15 21:20:21 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:20:21 up 57 min,  1 user,  load average: 0.02, 0.10, 0.07 23f201997c908759b1f399892d2ccc418552e12b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -23,7 +23,7 @@ void raise_intr(uint8_t intr_no)
     operand_write(&opr_dest);//Push(EIP);
     if(intr_no>=32)cpu.eflags.IF=0;// Clear IF if it is an interrupt
     uint32_t entry;// Trigger an exception/interrupt with 'intr_no'
-    IDTR temp=cpu.idtr+intr_no*8;// 'intr_no' is the index to the IDT
+    IDTR temp.base=cpu.idtr+intr_no*8;// 'intr_no' is the index to the IDT
     entry=temp.base+temp.limit;
     cpu.eip=entry;// Set EIP to the entry of the interrupt handler//跳转执行
 // Trigger an exception/interrupt with 'intr_no'
