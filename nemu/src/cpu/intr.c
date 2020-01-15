0e8e227de8e8d03a22b6773985dd29ac4b74be22
tracer-pa-public <tracer@njuics.org>
2020-01-15 22:17:34 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:17:34 up  1:54,  1 user,  load average: 0.14, 0.11, 0.07 cd05eb5fb2c878b77a72bd99298fae8ac213f390

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -13,13 +13,35 @@ void raise_intr(uint8_t intr_no)
     vaddr_write(cpu.esp,SREG_SS,2,cpu.cs.val);//Push(CS);
 	cpu.esp-=data_size/8;
     vaddr_write(cpu.esp,SREG_SS,4,cpu.eip);//Push(EIP);
-    if(intr_no>=32)cpu.eflags.IF=0;// Clear IF if it is an interrupt
-    uint32_t entry;// Trigger an exception/interrupt with 'intr_no'
-    uint32_t temp=cpu.idtr.base+intr_no*8;// 'intr_no' is the index to the IDT
-    uint32_t selector=temp>>16;
-    uint32_t offset=temp;
-    entry=selector+offset;
-    cpu.eip=entry;// Set EIP to the entry of the interrupt handler//跳转执行
+
+
+    SegDesc temp;// load cs
+    memcpy(&temp,hw_mem+cpu.gdtr.base+(cpu.cs.index*8),8);
+    uint32_t base_31_24 =temp.base_31_24;
+    uint32_t base_23_16 =temp.base_23_16 ;
+    uint32_t base_15_0=temp.base_15_0;
+    base_31_24<<=24;
+    base_23_16<<=16;
+    uint32_t bs=base_15_0|base_23_16;
+    bs=bs|base_31_24;
+
+    uint32_t limit_15_0=temp.limit_15_0;
+    uint32_t limit_19_16=temp.limit_19_16;
+    limit_19_16<<=16;
+    uint32_t lmt=limit_19_16|limit_15_0;
+    cpu.cs.base=bs;
+    cpu.cs.limit=lmt;
+    cpu.cs.type=temp.type;
+    cpu.cs.privilege_level=temp.privilege_level;
+    cpu.cs.soft_use=temp.soft_use;
+
+
+    if(intr_no==15)cpu.eflags.IF=0;// Clear IF if it is an interrupt
+
+    GateDesc desc;// Trigger an exception/interrupt with 'intr_no'
+    memcpy(&desc,hw_mem+cpu.idtr.base+(intr_no*8),8);// 'intr_no' is the index to the IDT
+    cpu.eip=desc.offset_15_0|(offset_31_16<<16);
+// Set EIP to the entry of the interrupt handler//跳转执行
 // Trigger an exception/interrupt with 'intr_no'
 // 'intr_no' is the index to the IDT
 // Push EFLAGS, CS, and EIP
