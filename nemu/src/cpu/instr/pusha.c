5bff3ec3e67434d4404e64a4385d9806b5c9a41c
tracer-pa-public <tracer@njuics.org>
2020-01-15 21:10:50 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:10:50 up 47 min,  1 user,  load average: 0.20, 0.09, 0.03 bda59a6eeaedc0fae496fed8aea2cea543f4a61f

--- nemu/src/cpu/instr/pusha.c
+++ nemu/src/cpu/instr/pusha.c
@@ -2,38 +2,7 @@
 
 make_instr_func(pusha)
 {
-	if(data_size==16){
-    uint16_t temp=cpu.esp;//Temp ← (ESP);
-	cpu.sp-=data_size/8;
-    opr_dest.data_size=data_size;
-    opr_dest.type=OPR_REG;
-    opr_dest.addr=cpu.sp;
-    opr_dest.sreg=SREG_SS;
-    opr_dest.val=cpu.ax;
-    operand_write(&opr_dest);//Push(AX);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.cx;
-    operand_write(&opr_dest);//Push(CX);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.dx;
-    operand_write(&opr_dest);//Push(DX);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.bx;
-    operand_write(&opr_dest);//Push(BX);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.temp;
-    operand_write(&opr_dest);//Push(Temp);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.bp;
-    operand_write(&opr_dest);//Push(BP);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.si;
-    operand_write(&opr_dest);//Push(SI);
-	cpu.sp-=data_size/8;opr_dest.addr=cpu.sp;
-    opr_dest.val=cpu.di;
-    operand_write(&opr_dest);//Push(DI);      
-    }
-    else if(data_size==32){
+	
     uint32_t temp=cpu.esp;//Temp ← (ESP);
 	cpu.esp-=data_size/8;
     opr_dest.data_size=data_size;
@@ -64,7 +33,6 @@ make_instr_func(pusha)
 	cpu.esp-=data_size/8;opr_dest.addr=cpu.esp;
     opr_dest.val=cpu.edi;
     operand_write(&opr_dest);//Push(EDI);
-    }
     print_asm_0("pusha", "", 1);
     return 1;
     
