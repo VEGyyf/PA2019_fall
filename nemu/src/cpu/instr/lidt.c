893a7e289cdf71c0ba33028543e2671b2bc247b7
tracer-pa-public <tracer@njuics.org>
2020-01-17 23:49:39 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:49:39 up 4 min,  1 user,  load average: 0.02, 0.13, 0.07 758a0d53b1217d8eab6d715a352f26e064e270b6

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -4,20 +4,28 @@
 
 make_instr_func(lidt)
 {
-    OPERAND m;
-	m.data_size = data_size;
+     OPERAND m;
+    modrm_rm(eip+1,&m);
+    cpu.gdtr.limit=laddr_read(m.addr,2);
+    cpu.gdtr.base=laddr_read(m.addr+2,4);//bs;  
+	/*m.data_size = data_size;
     m.sreg=SREG_ES;
     m.addr=eip+2;
     m.type=OPR_IMM;
+	//int len=2;
+    //len+=modrm_rm(eip+2,&m);
 	operand_read(&m);
-    cpu.gdtr.limit=paddr_read(m.val,2);
+    //uint32_t lmt=0;
+    //memcpy(&lmt,hw_mem+m.val,2);
+    cpu.gdtr.limit=paddr_read(m.val,2);//lmt;
     uint32_t bs=0;
     if(data_size==16){
-        cpu.gdtr.base=paddr_read(m.val+16,3);
+        cpu.gdtr.base=paddr_read(m.val+16,3);//memcpy(&bs,hw_mem+m.val+16,3);
         memset(&bs+3,0,1);
     }
-    else if(data_size==32)
+    else if(data_size==32)//memcpy(&bs,hw_mem+16,4);
     cpu.gdtr.base=paddr_read(m.val+16,4);//bs;   
-	print_asm_1("lgdt", "", 2+data_size/8, &m);
+    //assert(lmt==0xfffff);
+	print_asm_1("lgdt", "", 2+data_size/8, &m);*/
 	return 2+data_size/8;
 }
