112fb5c966b30f20c6268370c4fa0b674ec0b0aa
monitor <tracer@njuics.org>
2020-01-18 00:10:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -4,8 +4,7 @@
 make_instr_func(lidt)
 {
     OPERAND m;
-    int len=2;
-    len+=modrm_rm(eip+1,&m);
+    modrm_rm(eip+1,&m);
     cpu.gdtr.limit=laddr_read(m.addr,2);
     cpu.gdtr.base=laddr_read(m.addr+2,4);//bs;  
 	/*m.data_size = data_size;
