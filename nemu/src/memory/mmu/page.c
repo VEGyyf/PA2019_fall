f1efc4efca81fe5cc6db7d192fc7542432066dbb
tracer-pa-public <tracer@njuics.org>
2020-01-17 23:54:30 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:54:30 up 9 min,  1 user,  load average: 0.17, 0.23, 0.13 47f80e28facea6343b099409a865ff09b19f63f

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,7 +9,7 @@ paddr_t page_translate(laddr_t laddr)
     uint32_t dir_offset=dir<<2;
     uint32_t dir_bs=cpu.cr3.pdbr<<12;
     PDE temp_dir;//memcpy?
-    temp_dir.val=paddr_read(dir_bs+dir_offset,4);//memcpy(&temp_dir,hw_mem+dir_bs+dir_offset,4);//uint32_t?4B?
+    temp_dir.val=laddr_read(dir_bs+dir_offset,4);//memcpy(&temp_dir,hw_mem+dir_bs+dir_offset,4);//uint32_t?4B?
 
     uint32_t page_frame=temp_dir.page_frame;
     uint32_t pg_bs=page_frame<<12;
