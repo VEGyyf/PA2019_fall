d336e9f377d1f8e351323a8e4d8592b5e2cf11a7
tracer-pa-public <tracer@njuics.org>
2020-01-18 00:22:25 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:22:25 up 37 min,  1 user,  load average: 0.08, 0.10, 0.17 d1384d0b33da026ecb826207c1c37ff2acb36d78

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -5,14 +5,14 @@ static void instr_execute_1op()
 {
    
 	operand_read(&opr_src);
-	cpu.esp-=data_size/8;
+	/*cpu.esp-=data_size/8;
     opr_dest.data_size=data_size;
     opr_dest.sreg=SREG_SS;
     opr_dest.type=OPR_REG;
     opr_dest.addr=cpu.esp;
     opr_dest.val=opr_src.val;
     //len + = modrm_r_rm(eip + 1, &opr_src, &opr_dest); 
-    operand_write(&opr_dest);
+    operand_write(&opr_dest);*/
 
     cpu.esp-=data_size/8;
     vaddr_write(cpu.esp,SREG_SS,4,opr_src.val);
