9d922214111974f60da5beddb6d28332244a42d3
tracer-pa-public <tracer@njuics.org>
2020-01-20 11:49:47 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:49:47 up  1:44,  1 user,  load average: 0.11, 0.12, 0.09 76c445c12738750520ca1360667ac1aa276c962e

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -17,15 +17,15 @@ make_instr_func(in_b)
         dst.val=pio_read(port, 1);
         operand_write(&dst);
         print_asm_2("in", "_b", len, &src, &dst);
-      return 3;
+      return len;
 }
 
 make_instr_func(in_v)
-{	    OPERAND src, dst;
+{	    int len=1;
+        OPERAND src, dst;
         src.data_size = data_size;
         dst.data_size = data_size;
-        src.addr=eip+1;
-        dst.addr=eip+2;
+        len += modrm_r_rm(eip + 1, &dst, &src);
         src.sreg=SREG_SS;
         dst.sreg=SREG_SS;
         src.type=OPR_REG;
@@ -36,6 +36,6 @@ make_instr_func(in_v)
        // else if(data_size==32)dst.val=in_long(port);
         dst.val=pio_read(port, data_size/8);
         operand_write(&dst);
-        print_asm_2("in", "_v", 3, &src, &dst);
-      return 3;
+        print_asm_2("in", "_v", len, &src, &dst);
+      return len;
 }
