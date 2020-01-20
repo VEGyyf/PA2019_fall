4de85703487349ee64fb4aeb1bec205df8effffd
monitor <tracer@njuics.org>
2020-01-20 11:43:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -4,7 +4,10 @@
 
 make_instr_func(in_b)
 {	
-        OPERAND src, dst;
+       	operand_read(&opr_src);
+    uint16_t port=src.val;
+	opr_dest.val = pio_read(port, 1);
+	operand_write(&opr_dest);/* OPERAND src, dst;
         src.data_size = data_size;
         dst.data_size = data_size;
         src.sreg=SREG_SS;
@@ -14,13 +17,16 @@ make_instr_func(in_b)
         operand_read(&src);
         uint16_t port=src.val;
         dst.val=pio_read(port, 1);
-        operand_write(&dst);
-        print_asm_2("in", "_b", 3, &src, &dst);
+        operand_write(&dst);*/
+        print_asm_2("in", "_b", 3, &opr_src, &opr_dst);
       return 3;
 }
 
 make_instr_func(in_v)
-{	 OPERAND src, dst;
+{	    	operand_read(&opr_src);
+    uint16_t port=src.val;
+	opr_dest.val = pio_read(port, data_size/8);
+	operand_write(&opr_dest);/*OPERAND src, dst;
         src.data_size = data_size;
         dst.data_size = data_size;
         src.sreg=SREG_SS;
@@ -32,7 +38,7 @@ make_instr_func(in_v)
         //if(data_size==16)dst.val=in_word(port);
        // else if(data_size==32)dst.val=in_long(port);
         dst.val=pio_read(port, data_size/8);
-        operand_write(&dst);
+        operand_write(&dst);*/
         print_asm_2("in", "_v", 3, &src, &dst);
       return 3;
 }
