0f3a3e18069c0209cf11ac1d6f6435aaa42cc56a
monitor <tracer@njuics.org>
2020-01-20 11:12:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -12,14 +12,16 @@ make_instr_func(out_b)
         src.type=OPR_REG;
         dst.type=OPR_REG;
         operand_read(&src);
+        operand_read(&dst);
+        uint8_t data=src.val;
         uint16_t port=dst.val;
-        out_byte(uint16_t port, uint8_t data)
-        operand_write(&dst);
-      return 3;
+        out_byte(port, data);
+        return 3;
 }
 
 make_instr_func(out_v)
 {	
+       
         OPERAND src, dst;
         src.data_size = data_size;
         dst.data_size = data_size;
@@ -28,8 +30,15 @@ make_instr_func(out_v)
         src.type=OPR_REG;
         dst.type=OPR_REG;
         operand_read(&src);
-        uint16_t port=src.val;
-        dst.val=in_byte(port);
-        operand_write(&dst);
-      return 3;
+        operand_read(&dst);
+        uint16_t port=dst.val;
+        if(data_size==16){
+        uint16_t data=src.val;
+        out_byte(port, data);
+        }
+        else if(data_size==32){
+        uint32_t data=src.val;
+        out_byte(port, data);
+        }
+        return 3;
 }
