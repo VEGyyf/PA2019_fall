0406e9691d7660ecf2414e2637192d45c96f105b
monitor <tracer@njuics.org>
2020-01-20 10:34:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -1,6 +1,14 @@
 #include "cpu/instr.h"
 #include "kernel/include/x86/io.h"
+#include "device/port_io.h"
 
 make_instr_func(in)
 {	
+in_byte(uint16_t port){
+	uint8_t data;
+	asm volatile("in %1, %0"
+				 : "=a"(data)
+				 : "d"(port));
+	return data;
+}
 }
