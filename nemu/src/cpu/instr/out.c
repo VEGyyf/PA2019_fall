7b69b3487e5141e09023a6ba141180780be90770
tracer-pa-public <tracer@njuics.org>
2020-01-20 11:26:59 +0800

    >  nemu 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:26:59 up  1:22,  1 user,  load average: 0.23, 0.13, 0.08 d2773da041342654c9358d14432c229b6e2da94

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -5,7 +5,7 @@
 make_instr_func(out_b)
 {	
         OPERAND src, dst;
-        src.data_size = 8;
+        src.data_size = data_size;
         dst.data_size = data_size;
         src.sreg=SREG_SS;
         dst.sreg=SREG_SS;
@@ -15,7 +15,7 @@ make_instr_func(out_b)
         operand_read(&dst);
         uint32_t data=src.val;
         uint16_t port=dst.val;
-        pio_write(port, size_t len, data)
+        pio_write(port, 1, data);
         return 3;
 }
 
@@ -32,13 +32,15 @@ make_instr_func(out_v)
         operand_read(&src);
         operand_read(&dst);
         uint16_t port=dst.val;
-        if(data_size==16){
+        uint32_t data=src.val;
+        /*if(data_size==16){
         uint16_t data=src.val;
         out_byte(port, data);
         }
         else if(data_size==32){
         uint32_t data=src.val;
         out_byte(port, data);
-        }
+        }*/
+        pio_write(port, data_size/8, data);
         return 3;
 }
