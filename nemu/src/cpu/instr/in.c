e51381817e7784664e6155b197ed014b9809310c
monitor <tracer@njuics.org>
2020-01-20 10:56:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -4,7 +4,18 @@
 
 make_instr_func(in_b)
 {	
-    in_byte(uint16_t port)
+        OPERAND src, dst;
+        src.data_size = data_size;
+        dst.data_size = 8;
+        src.sreg=SREG_SS;
+        dst.sreg=SREG_SS;
+        src.type=OPR_REG;
+        dst.type=OPR_REG;
+        operand_read(&src);
+        uint16_t port=src.val;
+        dst.val=in_byte(port);
+        operand_write(&dst);
+      return 3;
 }
 
 make_instr_func(in_v)
