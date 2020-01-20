51ff113bb027f022b7d105b534985ca5b63e8ef7
monitor <tracer@njuics.org>
2020-01-20 13:11:01 +0800

    git monitor commit.

--- kernel/src/lib/serial.c
+++ kernel/src/lib/serial.c
@@ -14,7 +14,7 @@ serial_idle(void)
 void serial_printc(char ch)
 {
 	while (!serial_idle())
-		; // wait untile serial is idle
+		; // wait until serial is idle
     uint16_t port=0x3F8;
     uint8_t data=(int)ch;
 	out_byte(port, data);// print 'ch' via out instruction here
