383454322df5f27ce0dc5a425caba228a3455e39
monitor <tracer@njuics.org>
2020-01-20 10:13:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -1,4 +1,5 @@
 #include "cpu/instr.h"
+#include "kernel/include/x86/io.h"
 
 make_instr_func(in)
 {	
--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -1,5 +1,7 @@
 #include "cpu/instr.h"
+#include "kernel/include/x86/io.h"
 
 make_instr_func(in)
 {	
+
 }
