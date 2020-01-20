f2141cf0cb5c020063ad3211ad1b0114f28b5f91
monitor <tracer@njuics.org>
2020-01-20 12:58:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -3,39 +3,16 @@
 #include "device/port_io.h"
 
 make_instr_func(in_b)
-{	    int len=1;
-        OPERAND src, dst;
-        src.data_size = data_size;
-        dst.data_size = data_size;
-        len += modrm_r_rm(eip + 1, &dst, &src);
-        src.sreg=SREG_SS;
-        dst.sreg=SREG_SS;
-        src.type=OPR_REG;
-        dst.type=OPR_REG;
-        operand_read(&src);
-        uint16_t port=src.val;
-        dst.val=pio_read(port, 1);
-        operand_write(&dst);
-        print_asm_2("in", "_b", len, &src, &dst);
-      return len;
+{	    
+        uint16_t port=cpu.edx&0xffff;
+        cpu.eax=pio_read(port, 1);      
+        print_asm_0("in_b", "", 1);
+        return 1;
 }
 
 make_instr_func(in_v)
-{	    int len=1;
-        OPERAND src, dst;
-        src.data_size = data_size;
-        dst.data_size = data_size;
-        len += modrm_r_rm(eip + 1, &dst, &src);
-        src.sreg=SREG_SS;
-        dst.sreg=SREG_SS;
-        src.type=OPR_REG;
-        dst.type=OPR_REG;
-        operand_read(&src);
-        uint16_t port=src.val;
-        //if(data_size==16)dst.val=in_word(port);
-       // else if(data_size==32)dst.val=in_long(port);
-        dst.val=pio_read(port, data_size/8);
-        operand_write(&dst);
-        print_asm_2("in", "_v", len, &src, &dst);
-      return len;
+{	    uint16_t port=cpu.edx&0xffff;
+        cpu.eax=pio_read(port, data_size/8);      
+        print_asm_0("in_v", "", 1);
+        return 1;
 }
