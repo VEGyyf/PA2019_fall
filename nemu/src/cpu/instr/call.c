2b30f331718cbfb397ee21dd7892e4d6f9ff59c3
tracer-pa-public <tracer@njuics.org>
2020-01-20 14:47:30 +0800

    >  clean 181860127 linuxer-pa Linux debian-10-pa-yyf 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:47:30 up  4:42,  1 user,  load average: 0.50, 0.46, 0.58 59ad311a97a60824ecf96bb2875a1a9a7c7b0d32

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -26,14 +26,20 @@ make_instr_func(call_near)
 
 make_instr_func(call_near_indirect)
 {
-     
-
-            operand_read(&opr_src);
+         operand_read(&opr_src);
     cpu.esp-=data_size/8;
     vaddr_write(cpu.esp,SREG_SS,4,opr_src.val);//push ret_addr
 
+        OPERAND obj;
+        obj.sreg = SREG_CS;
+        obj.data_size = data_size;
+        obj.addr=eip+1;
+
+        operand_read(&obj);
+   
+      
         //int offset = sign_ext(rel.val, data_size);
-        print_asm_1("call", "", len, &obj);
+        print_asm_1("call", "", 1 + data_size / 8, &obj);
 
         cpu.eip =obj.val;
 
